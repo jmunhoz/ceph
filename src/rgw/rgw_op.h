51292e85df7da3d19fb3848b19a0feeb39375a08
Javier M. Mellid <jmunhoz@igalia.com>
2015-09-01 14:04:10 +0200

    rgw: multipart uploads copy part support
    Add multipart uploads copy part feature.
    
    Fixes: #12790
    
    Signed-off-by: Javier M. Mellid <jmunhoz@igalia.com>

--- src/rgw/rgw_op.h
+++ src/rgw/rgw_op.h
@@ -633,7 +633,12 @@ public:
 
 class RGWCopyObj : public RGWOp {
 protected:
+  RGWAccessControlPolicy policy;
   RGWAccessControlPolicy dest_policy;
+  const char *copy_source;
+  const char *copy_source_range;
+  off_t copy_source_range_fst = 0;
+  off_t copy_source_range_lst = 0;
   const char *if_mod;
   const char *if_unmod;
   const char *if_match;
@@ -670,10 +675,21 @@ protected:
 
   time_t delete_at;
 
+  bufferlist bl_aux;
+
+  const char *supplied_md5_b64;
+  const char *supplied_etag;
+  const char *obj_manifest;
+  bool chunked_upload;
+
+  MD5 *user_manifest_parts_hash;
+
   int init_common();
 
 public:
   RGWCopyObj() {
+    copy_source = NULL;
+    copy_source_range = NULL;
     if_mod = NULL;
     if_unmod = NULL;
     if_match = NULL;
@@ -692,18 +708,30 @@ public:
     last_ofs = 0;
     olh_epoch = 0;
     delete_at = 0;
+    supplied_md5_b64 = NULL;
+    obj_manifest = NULL;
+    chunked_upload = false;
+    supplied_etag = NULL;
+    user_manifest_parts_hash = NULL;
   }
 
   static bool parse_copy_location(const string& src, string& bucket_name, rgw_obj_key& object);
 
   virtual void init(RGWRados *store, struct req_state *s, RGWHandler *h) {
     RGWOp::init(store, s, h);
+    policy.set_ctx(s->cct);
     dest_policy.set_ctx(s->cct);
   }
+
+  RGWPutObjProcessor *select_processor(RGWObjectCtx& obj_ctx, bool *is_multipart);
+  void dispose_processor(RGWPutObjProcessor *processor);
+
   int verify_permission();
   void pre_exec();
   void execute();
   void progress_cb(off_t ofs);
+  int get_data_cb(bufferlist& bl, off_t bl_ofs, off_t bl_len);
+  int get_data(string bucket_name, string object_name, off_t fst, off_t lst, bufferlist& bl);
 
   virtual int init_dest_policy() { return 0; }
   virtual int get_params() = 0;
